7d612db1b00633750c6f66fbf4cb1280655a6d23
fourside <fourside@gmail.com>
2024-02-24 23:31:31 +0900

    take one task by one cron event

--- src/run-tasks.ts
+++ src/run-tasks.ts
@@ -14,13 +14,14 @@ export async function runTasks(
     .select()
     .from(tasks)
     .orderBy(desc(tasks.createdAt))
-    .where(eq(tasks.status, "pending"));
+    .where(eq(tasks.status, "pending"))
+    .limit(1); // invoke one by one to avoid from timeout of lambda
 
   if (result.length === 0) {
     return;
   }
 
-  const payloads = result.map(convert);
+  const payload = convert(result[0]);
 
   const lambda = new LambdaClient({
     region: "ap-northeast-1",
@@ -30,27 +31,24 @@ export async function runTasks(
     },
   });
 
-  // invoke one by one to avoid from timeout of lambda
-  for (const payload of payloads) {
-    await db
-      .update(tasks)
-      .set({ status: "doing" })
-      .where(eq(tasks.id, payload.id));
-    const res = await lambda.send(
-      new InvokeCommand({
-        FunctionName: "podcast-lambda-spot-task",
-        Payload: JSON.stringify(payload),
-      }),
-    );
-    console.log(res);
-    if (res.StatusCode === 200) {
-      const prepare = db
-        .delete(tasks)
-        .where(eq(tasks.id, sql.placeholder("id")))
-        .prepare();
-      await prepare.execute({ id: payload.id });
-    } else {
-      console.error("fail to invoke lambda.", res.StatusCode, res.Payload);
-    }
+  await db
+    .update(tasks)
+    .set({ status: "doing" })
+    .where(eq(tasks.id, payload.id));
+  const res = await lambda.send(
+    new InvokeCommand({
+      FunctionName: "podcast-lambda-spot-task",
+      Payload: JSON.stringify(payload),
+    }),
+  );
+  console.log(res);
+  if (res.StatusCode === 200) {
+    const prepare = db
+      .delete(tasks)
+      .where(eq(tasks.id, sql.placeholder("id")))
+      .prepare();
+    await prepare.execute({ id: payload.id });
+  } else {
+    console.error("fail to invoke lambda.", res.StatusCode, res.Payload);
   }
 }
